e0d6b0f8dfbe659ae46b917f31082b51d5f67b38
Cameron Kisailus <camkisailus@gmail.com>
2023-02-15 23:20:04 -0500

    look_at full and pp_simple full ran

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #! /usr/bin/zsh
-for chunk_num in 9 10 11 12 13 14 15 16 17 18 19
+chunk="33"
+# for chunk_num in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29
+while [ $chunk -lt 56 ]
 do 
-    /home/cuhsailus/anaconda3/envs/THOR/bin/python /home/cuhsailus/Desktop/Research/22_academic_year/iTHOR-SFM/eval.py --chunk $chunk_num > pick_and_place_simple_{$chunk_num}_results.txt
+    echo "Executing chunk:" $chunk "at" $(date +%H:%M:%S)
+    /home/cuhsailus/anaconda3/envs/THOR/bin/python /home/cuhsailus/Desktop/Research/22_academic_year/iTHOR-SFM/eval.py --chunk $chunk >pp_simple_results_$chunk.txt
+    echo "Done with chunk:" $chunk "at" $(date +%H:%M:%S)
+    chunk=$[ $chunk + 1 ]
 done
